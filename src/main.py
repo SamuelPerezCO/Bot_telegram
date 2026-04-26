ce37db6dc9a69fe08d1fb3889968b22dfabd92f6
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-04-26 18:20:32 -0500

    F*ck

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-from telegram import Update
 from telegram.ext import ApplicationBuilder , CommandHandler , ContextTypes
+from telegram import Update
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+token = os.getenv("BOT_TOKEN")
 
 async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello World!")
@@ -8,7 +15,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
     
 
-application = ApplicationBuilder().token("shit").build()
+application = ApplicationBuilder().token(token).build()
 
 application.add_handler( CommandHandler("start" , say_hello ))
 
