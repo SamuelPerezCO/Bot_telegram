b3cb4b96bbcd308ab3617bfb54f37266c3abd407
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-04-26 18:13:13 -0500

    I didn't notice

--- src/main.py
+++ src/main.py
@@ -8,10 +8,8 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
     
 
-application = ApplicationBuilder().token("8484815467:AAFl3ct2eu96pUFyw8mFqk1XwAepIZSfD4Y").build()
+application = ApplicationBuilder().token("shit").build()
 
 application.add_handler( CommandHandler("start" , say_hello ))
-application.add_handler( CommandHandler("echo" , echo))
-
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
