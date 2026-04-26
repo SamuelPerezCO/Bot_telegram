43f336219ceacc7101cb18b9ad4ce516d1428ea1
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-04-26 17:45:37 -0500

    First 2 functions

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder , CommandHandler , ContextTypes
+
+async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello World!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+    
+
+application = ApplicationBuilder().token("8484815467:AAFl3ct2eu96pUFyw8mFqk1XwAepIZSfD4Y").build()
+
+application.add_handler( CommandHandler("start" , say_hello ))
+application.add_handler( CommandHandler("echo" , echo))
+
+
+application.run_polling(allowed_updates=Update.ALL_TYPES)
