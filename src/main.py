b49fddfa2451412234811a761acf03f3d18ce8fb
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-05-03 15:00:04 -0500

    deleting one line

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Controllers.todoController import TodoController
 from dotenv import load_dotenv
-import os
 
 load_dotenv() 
 
